14e350e2f26facbbb33281c142c983acf681c397
machoargentino123 <nahuelirazausta@gmail.com>
2021-04-07 15:20:07 -0300

    agrego nuevo modelo csvimportado3 a 5

--- pasaje/views.py
+++ pasaje/views.py
@@ -130,8 +130,8 @@ class ListaView3():
    
     def index(request):
         x = []
-        lista1 = CsvImportado1.objects.all()
-        lista2 = CsvImportado2.objects.all()  
+        lista1 = CsvImportado1.objects.all().values('id','grupo_asignado','estado','status_reason_hidden','tipo_incidencia')
+        lista2 = CsvImportado2.objects.all().values('id','grupo_asignado','estado','status_reason_hidden','tipo_incidencia')
         lista3 = CsvImportado3.objects.all().values('id','grupo_asignado','estado','status_reason_hidden','tipo_incidencia')
         lista4 = CsvImportado4.objects.all().values('id','grupo_asignado','estado','status_reason_hidden','tipo_incidencia')
         lista5 = CsvImportado5.objects.all().values('id','grupo_asignado','estado','status_reason_hidden','tipo_incidencia')
