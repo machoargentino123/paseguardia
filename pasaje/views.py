5714455a386002eadbe22e21c5215f782a8647da
machoargentino123 <nahuelirazausta@gmail.com>
2021-04-07 10:23:31 -0300

    agrego nuevo modelo csvimportado3 a 5

--- pasaje/views.py
+++ pasaje/views.py
@@ -123,7 +123,7 @@ class ListaView3(ListView):
     def get_queryset(self):
         valor =self.kwargs['pk'] # recupera el dato del PK.
 
-        lista = CsvImportado2.objects.filter(
+        lista = CsvImportado3.objects.filter(
             celula_n = valor, 
             )  
         return lista
