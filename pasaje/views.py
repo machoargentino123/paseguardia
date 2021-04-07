47a505bf5d34170f409cbe282d47bf15115d3120
machoargentino123 <nahuelirazausta@gmail.com>
2021-04-07 15:58:37 -0300

    agrego nuevo modelo csvimportado3 a 5

--- pasaje/views.py
+++ pasaje/views.py
@@ -156,10 +156,10 @@ class ListaView3():
         listaa = CsvImportado1.objects.filter(id__in = x).values('id','grupo_asignado','estado','status_reason_hidden','tipo_incidencia')
         listab = CsvImportado2.objects.filter(id__in = x).values('id','grupo_asignado','estado','status_reason_hidden','tipo_incidencia')
         
-        lista = chain(listaa,listab)
-        print(lista)
+        lista = list(chain(listaa,listab))
+        print(lista[0])
 
-        context = {'lista':listaa,
+        context = {'lista':lista[0],
                    'fecha' : fecha,
                   }
 
