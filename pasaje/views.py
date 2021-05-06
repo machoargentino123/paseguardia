d5f0cb50c306296ca61cb41d4dce5b9c18d7c715
machoargentino123 <nahuelirazausta@gmail.com>
2021-05-06 17:06:53 -0300

    cambio en eventos

--- pasaje/views.py
+++ pasaje/views.py
@@ -492,7 +492,7 @@ class ListarColgados():
         colgados = Eventostkt.objects.values('sk','id','grupo_asignado','horario','estado').filter(
             sk__in = sk
         )
-        palabra_clave = self.request.GET.get('kword', '')
+        palabra_clave = request.GET.get('kword', '')
         print(palabra_clave)
         context = {'colgados' : colgados}
 
